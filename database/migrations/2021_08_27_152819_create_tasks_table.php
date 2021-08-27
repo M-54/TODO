d1ac1f116ae01e10af28d38602e5ee9e3689d2f8
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-27 16:40:52 +0430

    tasks migration defined

---
+++ database/migrations/2021_08_27_152819_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->string('description');
+            $table->foreignId('owner')->references('id')->on('users')->cascadeOnDelete();
+            $table->timestamp('deadline');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
