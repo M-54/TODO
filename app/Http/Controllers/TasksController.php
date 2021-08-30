797c1e2aa3b5671767b513ac19e532ae1538dc0c
mohsen <mohsen.roustapour@gmial.com>
2021-08-30 11:30:17 +0430

    first commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tasks;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tasks  $tasks
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tasks $tasks)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tasks  $tasks
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tasks $tasks)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tasks  $tasks
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tasks $tasks)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tasks  $tasks
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tasks $tasks)
+    {
+        //
+    }
+}
